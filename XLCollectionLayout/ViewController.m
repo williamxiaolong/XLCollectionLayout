9a2eeb08075d1d214399cf62fb2fcde482c6bf4b
yuanxiaolong <1198402764@qq.com>
2018-02-27 00:49:07 +0800

    git提交测试一下

--- XLCollectionLayout/ViewController.m
+++ XLCollectionLayout/ViewController.m
@@ -32,6 +32,7 @@
     
     [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([XLPhotoCell class]) bundle:nil] forCellWithReuseIdentifier:@"cell"];
     
+    // 测试git
 }
 
 #pragma mark - UICollectionViewDataSource
